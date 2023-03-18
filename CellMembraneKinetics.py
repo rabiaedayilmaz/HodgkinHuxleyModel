2b020278bdcf5506977e08537e473103d0e0d4ab
cuteopamp <edayilmxz@outlook.com>
2023-03-18 18:02:20 +0300

    init commit

---
+++ CellMembraneKinetics.py
@@ -0,0 +1,77 @@
+import numpy as np
+
+#########################################################
+# Big thanks to @slarson                                #
+# https://gist.github.com/slarson/37463b35ef8606629d2e  #
+#########################################################
+
+class MembraneKinetics:
+    """ Implements kinetic equations for membrane """
+    def __init__(self, C_m=1.0,
+                 g_Na=120.0, g_K=36.0, g_L=0.3,
+                 E_Na=50.0, E_K=-77.0, E_L=-54.387,
+                 t=np.arange(0.0, 400.0, 0.1),
+                 membrane_voltage=-65, m=0.05, h=0.6, n=0.32,
+                 ):
+        self.C_m = C_m
+        self.g_Na = g_Na
+        self.g_K = g_K
+        self.g_L = g_L
+        self.E_Na = E_Na
+        self.E_K = E_K
+        self.E_L = E_L
+        self.t = t
+        self.membrane_coef = [membrane_voltage, m, h, n]
+
+    # channel gating kinetics equations
+    @staticmethod
+    def alpha_m(V):
+        return 0.1 * (V + 40.0) / (1.0 - np.exp(-(V + 40.0) / 10.0))
+
+    @staticmethod
+    def beta_m(V):
+        return 4.0 * np.exp(-(V + 65.0) / 18.0)
+
+    @staticmethod
+    def alpha_h(V):
+        return 0.07 * np.exp(-(V + 65.0) / 20.0)
+
+    @staticmethod
+    def beta_h(V):
+        return 1.0 / (1.0 + np.exp(-(V + 35.0) / 10.0))
+
+    @staticmethod
+    def alpha_n(V):
+        return 0.01 * (V + 55.0) / (1.0 - np.exp(-(V + 55.0) / 10.0))
+
+    @staticmethod
+    def beta_n(V):
+        return 0.125 * np.exp(-(V + 65) / 80.0)
+
+
+class MembraneCurrents:
+    """ Implements current equations for membrane """
+    def __init__(self, g_Na, E_Na, g_K, E_K, g_L, E_L):
+        self.g_Na = g_Na
+        self.E_Na = E_Na
+        self.g_K = g_K
+        self.E_K = E_K
+        self.g_L = g_L
+        self.E_L = E_L
+
+    # Membrane current of Sodium
+    def I_Na(self, V, m, h):
+        return self.g_Na * m ** 3 * h * (V - self.E_Na)
+
+    # Membrane current of Potassium
+    def I_K(self, V, n):
+        return self.g_K * n ** 4 * (V - self.E_K)
+
+    # Membrane Leakage Current (because of Chlorine)
+    def I_L(self, V):
+        return self.g_L * (V - self.E_L)
+
+    # External/Injected Current Applied to Membrane
+    def I_inj(self, t):
+        return 10 * (t > 100) - 100 * (t > 200) + 35 * (t > 300)
+
---
+++ FireNeuron.py
@@ -0,0 +1,95 @@
+import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+
+from CellMembraneKinetics import MembraneKinetics, MembraneCurrents
+
+mbk = MembraneKinetics()
+
+###### MEMBRANE KINETICS VARIABLES ###########
+C_m = mbk.C_m                                #
+                                             #
+g_Na = mbk.g_Na                              #
+g_K = mbk.g_K                                #
+g_L = mbk.g_L                                #
+                                             #
+E_Na = mbk.E_Na                              #
+E_K = mbk.E_K                                #
+E_L = mbk.E_L                                #
+                                             #
+t = mbk.t                                    #
+                                             #
+##############################################
+
+mbc = MembraneCurrents(g_Na, E_Na, g_K, E_K, g_L, E_L)
+
+def dALLdt(X, t):
+    """
+     X: An array contains [V, m, n, h]
+     t: time range array
+
+     V:         Voltage across membrane
+     m and n:   controls Na+ channel
+     h:         controls K+ gate
+
+     m, n, h :  `gating variable`, probability that a channel is open at a given moment in time
+
+     Since V, m, h, n are changes by time
+     It calculates derivatives of V, m, h, and n
+    """
+    V, m, h, n = X  # extract values
+
+    # calculate membrane potential & activation variables
+    dVdt = (mbc.I_inj(t) - mbc.I_Na(V, m, h) - mbc.I_K(V, n) - mbc.I_L(V)) / C_m
+    dmdt = mbk.alpha_m(V) * (1.0 - m) - mbk.beta_m(V) * m
+    dhdt = mbk.alpha_h(V) * (1.0 - h) - mbk.beta_h(V) * h
+    dndt = mbk.alpha_n(V) * (1.0 - n) - mbk.beta_n(V) * n
+
+    return dVdt, dmdt, dhdt, dndt
+
+class Neuron:
+    """ Fires neuron and plot graphs """
+    def __init__(self, membrane_coef=mbk.membrane_coef, t=mbk.t):
+        X = odeint(dALLdt, membrane_coef, t)    # takes integral to find new values of V, m, h, n
+
+        # extract values from array
+        V = X[:, 0]
+        m = X[:, 1]
+        h = X[:, 2]
+        n = X[:, 3]
+
+        # calculate current for each channel Na+, K+, and leakage Cl-
+        ina = mbc.I_Na(V, m, h)
+        ik = mbc.I_K(V, n)
+        il = mbc.I_L(V)
+
+        # plot the signal
+        self.plot_signal(V, t, m, h, n, ina, ik, il)
+
+    def plot_signal(self, V, t, m, h, n, ina, ik, il):
+        """ Plot the currents, gating variables, injected current """
+        plt.title('Hodgkin-Huxley Neuron')
+        plt.plot(t, V, 'k')
+        plt.ylabel('V (mV)')
+        plt.show()
+
+        plt.plot(t, ina, 'c', label='$I_{Na}$')
+        plt.plot(t, ik, 'y', label='$I_{K}$')
+        plt.plot(t, il, 'm', label='$I_{L}$')
+        plt.ylabel('Current')
+        plt.legend()
+        plt.show()
+
+        plt.plot(t, m, 'r', label='m')
+        plt.plot(t, h, 'g', label='h')
+        plt.plot(t, n, 'b', label='n')
+        plt.ylabel('Gating Value')
+        plt.legend()
+        plt.show()
+
+        plt.plot(t, mbc.I_inj(t), 'k')
+        plt.xlabel('t (ms)')
+        plt.ylabel('$I_{inj}$ ($\\mu{A}/cm^2$)')
+        plt.ylim(-1, 31)
+        plt.show()
+
+Neuron()
